eee02732d8f47d8c2ae0929661d130bec7c79efa
harshitha1869 <23b01a12h7@svecw.edu.in>
2026-03-26 12:41:46 +0530

    fix api url

--- interviewflux1-frontend/src/environments/environment.prod.ts
+++ interviewflux1-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://interviewflux-production.up.railway.app'
+  apiUrl: 'https://interviewflux.onrender.com'
 };
--- interviewflux1-frontend/src/environments/environment.ts
+++ interviewflux1-frontend/src/environments/environment.ts
@@ -1,8 +1,5 @@
-//export const environment = {
-  //apiUrl: 'http://localhost:9090'
-//};
 
 export const environment = {
   production: true,
-  apiUrl: "https://interviewflux-production.up.railway.app"
+  apiUrl: 'https://interviewflux.onrender.com'
 };
